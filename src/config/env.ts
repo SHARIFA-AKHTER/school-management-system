9d0a1101782c351a6bcfda0b90cd2c03c2b2f4f0
unknown <sr0589071@gmail.com>
2026-04-26 17:50:32 +0600

    create an catchAsync sendResponse and env.ts

---
+++ src/config/env.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+
+dotenv.config({ path: path.join(process.cwd(), '.env') });
+
+export const env = {
+  node_env: process.env.NODE_ENV,
+  port: process.env.PORT || 5000,
+  database_url: process.env.DATABASE_URL,
+  jwt_secret: process.env.JWT_SECRET,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS || 12,
+};
